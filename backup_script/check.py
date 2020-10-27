60223afa8cd5ccd96ae2360e2c8e70540d73582d
wadi <benarb_w@etna-alternance.net>
2020-10-27 02:23:18 +0100

    ca marche

---
+++ backup_script/check.py
@@ -0,0 +1,68 @@
+import os
+import functions as utils
+import time
+
+
+path = './public'
+path_backup = './backup'
+
+
+def compute_dir_index(path):
+    files = []
+    subdirs = []
+    index = {}
+
+    for root, dirs, filenames in os.walk(path):
+        for subdir in dirs:
+            subdirs.append(os.path.relpath(os.path.join(root, subdir), path))
+        for f in filenames:
+            files.append(os.path.relpath(os.path.join(root, f), path))
+        
+    for f in files:
+        index[f] = os.path.getmtime(os.path.join(path, f))
+    
+    content = dict(files=files, subdirs=subdirs, index=index)
+    return content
+    
+
+    
+def compute_diff(dir_base, dir_cmp):
+    data = {}
+    data['deleted'] = list(set(dir_cmp['files']) - set(dir_base['files']))
+    data['created'] = list(set(dir_base['files']) - set(dir_cmp['files']))
+    data['updated'] = []
+    data['deleted_dirs'] = list(set(dir_cmp['subdirs']) - set(dir_base['subdirs']))
+    for f in set(dir_cmp['files']).intersection(set(dir_base['files'])):
+        if dir_base['index'][f] != dir_cmp['index'][f]:
+            data['updated'].append(f)
+
+
+    return data
+
+
+while(True):
+    content_main = compute_dir_index(path)
+    content_backup = compute_dir_index(path_backup)
+    subdirs = content_main["subdirs"]
+    result = compute_diff(content_main, content_backup)
+
+    created_file = result["created"]
+    updated_file = result["updated"]
+    deleted_file = result["deleted"]
+    deleted_dir = result["deleted_dirs"]
+
+    utils.copy_to_backup(created_file, updated_file, subdirs)
+    utils.remove(deleted_file, deleted_dir)
+    print("...")
+    time.sleep(10)
+
+
+
+
+
+
+
+
+
+
+
---
+++ backup_script/functions.py
@@ -0,0 +1,37 @@
+from shutil import copy
+import os
+
+path = "./public"
+path_backup = './backup'
+
+def write_paths(path_file, content):
+    f = open("." + path_file, "w")
+    f.write(str(content))
+    f.close()
+
+def read_paths(path_file):
+    f = open("." + path_file, "r")
+    return (f.read()) 
+
+
+def copy_to_backup(created_files, updated_file, subdirs):
+    for subdir in subdirs:
+        new_path = path_backup + "/" + subdir
+        if not os.path.exists(new_path):
+            os.mkdir(new_path)
+    
+    for file in created_files:
+        copy(path + "/" + file, path_backup + "/" + file)
+    
+    for up_file in updated_file:
+        copy(path + "/" + up_file, path_backup + "/" + up_file)
+
+def remove(files, dirs):
+    for file in files:
+        path_file = path_backup + "/" + file
+        if os.path.exists(path_file):
+            os.remove(path_file)
+    for dir in dirs:
+        path_folder = path_backup + "/" + dir
+        if os.path.exists(path_folder):
+            os.rmdir(path_folder) 
